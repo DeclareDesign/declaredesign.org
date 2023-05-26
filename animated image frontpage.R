4549bce38458c3a2e4b53716a6a7f70ab28144ce
Graeme Blair <graemeblair@users.noreply.github.com>
2023-05-26 11:52:19 -0700

    new frontpage gif

--- animated image frontpage.R
+++ animated image frontpage.R
@@ -1,4 +1,3 @@
-
 library(tidyverse)
 library(DeclareDesign)
 library(patchwork)
@@ -22,11 +21,19 @@ estimands <- draw_estimands(design)
 
 for(i in 1:250) {
   
+  summary_df <- 
+    dat_list[[i]] |> 
+    group_by(Z_fct) |> 
+    summarise(tidy(lm_robust(Y ~ 1))) |> 
+    mutate(Y = estimate)
+  
   yz_plot <- 
     ggplot(dat_list[[i]]) + 
     aes(Z_fct, Y) + 
     geom_hline(yintercept = 0, lty = "dashed", color = "darkgray") + 
     geom_jitter(width = 0.05, color = gray(0.8)) + 
+    geom_point(data = summary_df, size = 4) +
+    geom_linerange(data = summary_df, aes(ymin = conf.low, ymax = conf.high)) +
     coord_cartesian(ylim = c(-3, 3)) + 
     labs(y = "Outcome variable (Y)", x = "Treatment condition (Z)", title = "Simulated data") + 
     theme_minimal()
@@ -73,28 +80,24 @@ for(i in 1:250) {
            diagnosand = 
              factor(diagnosand, 
                     levels = c("n_sims", "coverage", "power", "rmse", "bias"),
-                    labels = c("N sims", "Coverage", "Power", "RMSE", "Bias"))) 
-  
-  # text_plot <- 
-  #   ggplot(data = text_df) + 
-  #   geom_point(aes(x = estimate, y = diagnosand)) +
-  #   geom_errorbarh(aes(xmin = conf.low, xmax = conf.high, y = diagnosand), height = 0.1)  + 
-  #   labs(title = "Diagnosands", y = "", x = "") + 
-  #   theme_minimal() + 
-  #   theme(axis.title.x = element_blank(),
-  #         panel.grid.minor.x = element_blank(),
-  #         panel.grid.minor.y = element_blank(),
-  #         title = element_text(size = 10))
+                    labels = c("N sims", "Coverage", "Power", "RMSE", "Bias"))) |>
+    mutate(label = if_else(diagnosand == "N sims", DeclareDesign:::format_num(estimate, 0),
+                           DeclareDesign:::format_num(estimate, 2))) |>
+    pivot_longer(c(diagnosand, label)) |>
+    mutate(x = if_else(name == "diagnosand", -0.5, 0.5),
+           y = diagnosand_ID,
+           hjust = if_else(name == "Diagnosand", 0, 1)) 
   
   text_plot <- 
     ggplot(data = text_df) + 
-    geom_text(aes(label = round(estimate, 2), x = 0, y = diagnosand)) +
+    geom_text(aes(label = value, x = x, y = y, hjust = hjust)) +
     labs(title = "Diagnosands", y = "", x = "") + 
+    coord_cartesian(xlim = c(-1, 1)) +
     theme_minimal() + 
     theme(axis.title.x = element_blank(),
           axis.text.x = element_blank(),
           axis.ticks.x = element_blank(),
-          axis.text.y = element_text(size = 12),
+          axis.text.y = element_blank(),
           panel.grid.major.y = element_blank(),
           panel.grid.minor.y = element_blank(),
           panel.grid.major.x = element_blank(),
@@ -111,8 +114,26 @@ for(i in 1:250) {
   
 }
 
+i <- 250
+hist_plot <- 
+  ggplot(est_list |> bind_rows() |> slice(1:i)) + 
+  geom_histogram(aes(x = estimate, y = (after_stat(count))/sum(after_stat(count))), col = NA) + 
+  # geom_histogram(aes(x = est_list[[i]]$estimate, y = (after_stat(count))/sum(after_stat(count))), col = NA, fill = "lightblue") + 
+  scale_y_continuous("Percent of sampling distribution", labels = scales::percent) +
+  scale_x_continuous("") +
+  coord_flip(xlim = c(-2.2, 2.2), ylim = c(0, 0.1)) + 
+  labs(title = "Sampling distribution of estimates") + 
+  theme_minimal() +
+  theme(title = element_text(size = 10))
+
+gg <- (text_plot + yz_plot) / ( (diff_plot + hist_plot) + plot_layout(widths = c(1, 3))) 
+gg
+
+ggsave(gg, file = paste0("static/img/img", i, "-final.png"), width = 7, height = 5)
+
+
 library(gifski)
 
-png_files <- paste0("static/img/img", c(1:250, rep(250, 100)), ".png")
-gifski(png_files, "static/frontpage.gif", width = 4000, height = 4000, delay = 0.01)
+png_files <- c(paste0("static/img/img", c(1:250), ".png"), "static/img/img250-final.png")
+gifski(png_files, "static/frontpage.gif", width = 4000, height = 4000, delay = 0.01, loop = FALSE)
 
